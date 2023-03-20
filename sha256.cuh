0803904eaa69e9162ecb2d699a215129250224b1
geiccobs <geiccobs@icloud.com>
2023-03-20 18:11:22 +0100

    Really not something new but still avoiding some useless comments.

--- sha256.cuh
+++ sha256.cuh
@@ -1,6 +1,8 @@
 #ifndef SHA256_H
 #define SHA256_H
 
+#include <cstdint>
+
 
 /****************************** MACROS ******************************/
 #define SHA256_BLOCK_SIZE 32            // SHA256 outputs a 32 byte digest
@@ -26,14 +28,6 @@
 /**************************** DATA TYPES ****************************/
 typedef unsigned char BYTE;             // 8-bit byte
 typedef uint32_t  WORD;             // 32-bit word, change to "long" for 16-bit machines
-
-typedef struct JOB {
-	BYTE * data;
-	unsigned long long size;
-	BYTE digest[64];
-	char fname[128];
-}JOB;
-
 
 typedef struct {
 	BYTE data[64];
@@ -61,77 +55,48 @@ __device__ void sha256_init(SHA256_CTX *ctx);
 __device__ void sha256_update(SHA256_CTX *ctx, const BYTE data[], size_t len);
 __device__ void sha256_final(SHA256_CTX *ctx, BYTE hash[]);
 
-
-char * hash_to_string(BYTE * buff) {
-	char * string = (char *)malloc(70);
-	int k, i;
-	for (i = 0, k = 0; i < 32; i++, k+= 2)
-	{
-		sprintf(string + k, "%.2x", buff[i]);
-		//printf("%02x", buff[i]);
-	}
-	string[64] = 0;
-	return string;
-}
-
-void print_job(JOB * j){
-	printf("%s  %s\n", hash_to_string(j->digest), j->fname);
-}
-
-void print_jobs(JOB ** jobs, int n) {
-	for (int i = 0; i < n; i++)
-	{
-        print_job(jobs[i]);
-		// printf("@ %p JOB[%i] \n", jobs[i], i);
-		// printf("\t @ 0x%p data = %x \n", jobs[i]->data, (jobs[i]->data == 0)? 0 : jobs[i]->data[0]);
-		// printf("\t @ 0x%p size = %llu \n", &(jobs[i]->size), jobs[i]->size);
-		// printf("\t @ 0x%p fname = %s \n", &(jobs[i]->fname), jobs[i]->fname);
-		// printf("\t @ 0x%p digest = %s \n------\n", jobs[i]->digest, hash_to_string(jobs[i]->digest));
-	}
-}
-
 __device__ void mycpy12(uint32_t *d, const uint32_t *s) {
 #pragma unroll 3
-    for (int k=0; k < 3; k++) d[k] = s[k];
+	for (int k = 0; k < 3; k++) d[k] = s[k];
 }
 
 __device__ void mycpy16(uint32_t *d, const uint32_t *s) {
 #pragma unroll 4
-    for (int k=0; k < 4; k++) d[k] = s[k];
+	for (int k = 0; k < 4; k++) d[k] = s[k];
 }
 
 __device__ void mycpy32(uint32_t *d, const uint32_t *s) {
 #pragma unroll 8
-    for (int k=0; k < 8; k++) d[k] = s[k];
+	for (int k = 0; k < 8; k++) d[k] = s[k];
 }
 
 __device__ void mycpy44(uint32_t *d, const uint32_t *s) {
 #pragma unroll 11
-    for (int k=0; k < 11; k++) d[k] = s[k];
+	for (int k = 0; k < 11; k++) d[k] = s[k];
 }
 
 __device__ void mycpy48(uint32_t *d, const uint32_t *s) {
 #pragma unroll 12
-    for (int k=0; k < 12; k++) d[k] = s[k];
+	for (int k = 0; k < 12; k++) d[k] = s[k];
 }
 
 __device__ void mycpy64(uint32_t *d, const uint32_t *s) {
 #pragma unroll 16
-    for (int k=0; k < 16; k++) d[k] = s[k];
+	for (int k = 0; k < 16; k++) d[k] = s[k];
 }
 
 __device__ void sha256_transform(SHA256_CTX *ctx, const BYTE data[])
 {
 	WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
-    WORD S[8];
+	WORD S[8];
 
-    //mycpy32(S, ctx->state);
+	//mycpy32(S, ctx->state);
 
-    #pragma unroll 16
+#pragma unroll 16
 	for (i = 0, j = 0; i < 16; ++i, j += 4)
 		m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
 
-    #pragma unroll 64
+#pragma unroll 64
 	for (; i < 64; ++i)
 		m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
 
@@ -144,7 +109,7 @@ __device__ void sha256_transform(SHA256_CTX *ctx, const BYTE data[])
 	g = ctx->state[6];
 	h = ctx->state[7];
 
-    #pragma unroll 64
+#pragma unroll 64
 	for (i = 0; i < 64; ++i) {
 		t1 = h + EP1(e) + CH(e, f, g) + dev_k[i] + m[i];
 		t2 = EP0(a) + MAJ(a, b, c);
